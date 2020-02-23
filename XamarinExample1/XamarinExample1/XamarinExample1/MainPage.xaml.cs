88b23c3a1ab7acd413b2f34ee326143629e8df64
Ebrar Tepeciklioglu <tepecikliogluebrar@gmail.com>
2020-02-23 03:29:19 +0300

    Xamarin Examples1 add.

---
+++ XamarinExample1/XamarinExample1/XamarinExample1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinExample1
+{
+    
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonName_OnClicked(object sender,EventArgs e)
+        {
+
+            string text = MainNameEntry.Text;
+
+            MainNameLabel.Text = "Hello" +" "+ text;
+        }
+    }
+}
